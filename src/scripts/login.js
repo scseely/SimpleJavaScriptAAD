2bac81df2f0e1538414677f3c01cd2f7ab34dcd0
Scott Seely <scseely@microsoft.com>
2020-11-12 14:20:30 -0800

    Updated to 2.0

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,21 +1,31 @@
 const AAD_TOKEN = "id_token";
 const MARKETPLACE_TOKEN = "token";
 const MARKETPLACE_TOKEN_STORAGE = "az_marketplace_token";
-window.AdalContext = new AuthenticationContext({
-    clientId : '<your client id here>'
-});
+
+aadConfig = {
+    clientId : '<your Application (client) id from AAD>',
+    tenant: 'common',
+    scope: 'openid+profile+email'
+
+};
 
 function displayJwtInfo(jwtToken){
     // TODO: make this "your own"
     $("h1").text(jwtToken.name);
     $("#tenantId").text(jwtToken.tid);
-    $("#email").text(jwtToken.upn);
+    $("#email").text(jwtToken.preferred_username);
+    $("#objectId").text(jwtToken.oid);
 }
 
-function displayMarketplaceInfo(marketplaceToken){
-    $("#marketplaceToken").text(marketplaceToken);
-
-    // TODO: resolve the marketplace token, probably via a backend
+function constructLoginUrl(){
+    let loginUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?" +
+        'client_id=' + encodeURIComponent(aadConfig.clientId) +
+        '&response_type=id_token' +
+        '&redirect_uri=' + window.location.href +
+        '&response_mode=fragment' +
+        '&scope=' + aadConfig.scope +
+        '&nonce=' + (Math.floor(Math.random*10000000));
+    return loginUrl;
 }
 
 $(
@@ -23,16 +33,16 @@ $(
       let pageUrl = new URL(window.location.href);
       let hashString = '?' + pageUrl.hash.substr(1);
       let hashParams = new URLSearchParams(hashString);
-      if (hashParams.has(AAD_TOKEN)) {
-          let marketplaceToken = localStorage.getItem(MARKETPLACE_TOKEN_STORAGE);
+      if (hashParams.has('error')){
+          alert(hashParams.get('error'));
+      }
+      else if (hashParams.has(AAD_TOKEN)) {
           let jwtInfo = hashParams.get(AAD_TOKEN);
           let jwtToken = jwt_decode(jwtInfo);
           displayJwtInfo(jwtToken);
-          displayMarketplaceInfo(marketplaceToken);
-      } else if (pageUrl.searchParams.has(MARKETPLACE_TOKEN)){
-          let marketplaceToken = pageUrl.searchParams.get(MARKETPLACE_TOKEN);
-          localStorage.setItem(MARKETPLACE_TOKEN_STORAGE, marketplaceToken);
-          AdalContext.login();
+      } else {
+          window.location.href = constructLoginUrl();
       }
   }
 );
+
